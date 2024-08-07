a8e8b815b993c46e1664d4e05acb4e3324749480
linannan <linannan@prosnav.com>
2024-08-07 10:27:49 +0800

    增加搜索功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { navbar } from './configs/navbar'
 import { sidebar } from './configs/sidebar'
+import { searchProPlugin } from "vuepress-plugin-search-pro"
 export default defineUserConfig({
   base: '/docs/',
   lang: 'zh',
@@ -13,11 +14,25 @@ export default defineUserConfig({
     logo: 'https://vuejs.press/images/hero.png',
 
     navbar: navbar,
-    sidebar: sidebar,
-    // 设置搜索框的样式
-    search: {
-      provider: "local"
-    },
+    sidebar: sidebar
   }),
+  // 搜索插件设置
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
   bundler: viteBundler()
 })
